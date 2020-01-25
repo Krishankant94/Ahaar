f502de6b9753b9e207ae91cbc8966589468d2916
Prakash Kumar <psingh5293@gmail.com>
2020-01-25 08:38:51 +0530

    route changes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,22 +1,17 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./components/main-components/home/Home";
-import DashBoard from "./components/main-components/dashboard/DashBoard";
-import Auth from "./components/main-components/auth/Authentication";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/main-components/home/Home';
+import DashBoard from './components/main-components/dashboard/DashBoard';
+import Auth from './components/main-components/auth/Authentication';
 
 const Routes = () => {
   return (
     <div className="container">
       <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/dashboard">
-          <DashBoard />
-        </Route>
-        <Route path="/auth">
-          <Auth />
-        </Route>
+        <Route path="/home" component={Home} />
+        <Route path="/dashboard" component={DashBoard} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={Home} />
       </Switch>
     </div>
   );
